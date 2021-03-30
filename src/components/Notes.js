259794ac3525b57849de49fb0aa977a0632a7959
NeroN-95 <romaburlaka1995@gmail.com>
2021-03-31 01:26:42 +0300

    creating components and pages

---
+++ src/components/Notes.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Notes = ({notes}) =>{
+    return(
+        <ul className="list-group">
+            {notes.map(note =>(
+                <li
+                    className="list-group-item note"
+                    key={note.id}
+                >
+                    {note.title}
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
+                    >
+                        &times;
+                    </button>
+                </li>
+            ))}
+        </ul>
+    )
+}
